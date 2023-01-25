64f0bdea0f97d1986f1b12910b4152faee19f1c8
Charllys de Lima <charllys.lima@outlook.com>
2023-01-24 21:35:20 -0300

    Implementado webpack

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// Load Styles
+import '../scss/main.scss';
+
+// Load Bootstrap init
+import {initBootstrap} from "./bootstrap.js";
+
+// Loading bootstrap with optional features
+initBootstrap({
+    tooltip: true,
+    popover: true,
+    toasts: true,
+});
+
